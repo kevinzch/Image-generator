98f9400a3b55530a530bc70b191e6fd5d582aea6
kevinzch <a645749023@gmail.com>
2022-01-09 17:06:54 +0900

    Add feature: Generate images with all images in input folder and save to output folder

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -1,22 +1,48 @@
+from os import listdir, path
 from PIL import Image
 
 # Enlarge scale factor
 SCALE_FACTOR = 4
 
-# Open template image
-img_template = Image.open(r"D:\01_Dev\ImageGenerator\Tamplate.jpg")
+# Input folder path
+# Put overlay images here
+PATH_INPUT_FOLDER = r"D:\01_Dev\image-generator\input"
 
-# Open overlay image
-img_overlay = Image.open(r"D:\01_Dev\ImageGenerator\header.jpg")
+# Output folder path
+# Generated images will be saved here
+PATH_OUTPUT_FOLDER = r"D:\01_Dev\image-generator\output"
 
-# Enlarge header to 400%
-img_overlay_resize = img_overlay.resize((img_overlay.width * SCALE_FACTOR, img_overlay.height * SCALE_FACTOR))
+overlay_image_path_list = []
 
-# Paste header image at the center of template image
-img_template.paste(img_overlay_resize, (img_template.width // 2 - img_overlay_resize.width // 2, img_template.height // 2 - img_overlay_resize.height // 2))
+# Read files from input folder
+def read_overlay_images():
+    local_overlay_files = listdir(PATH_INPUT_FOLDER)
 
-# Save image
-img_template.save(r"D:\01_Dev\ImageGenerator\item_img.jpg")
+    for temp_file in local_overlay_files:
+        overlay_image_path_list.append(path.join(PATH_INPUT_FOLDER, temp_file))
 
-# Displaying the image
-img_template.show()
+# Generate images
+def generate_images():
+    local_cnt = 0
+
+    for temp_path in overlay_image_path_list:
+        # Open template image
+        img_template = Image.open(r"D:\01_Dev\image-generator\template.jpg")
+
+        # Open overlay image
+        img_overlay = Image.open(temp_path)
+
+        # Enlarge header to 400%
+        img_overlay_resize = img_overlay.resize((img_overlay.width * SCALE_FACTOR, img_overlay.height * SCALE_FACTOR))
+
+        # Paste header image at the center of template image
+        img_template.paste(img_overlay_resize, (img_template.width // 2 - img_overlay_resize.width // 2, img_template.height // 2 - img_overlay_resize.height // 2))
+
+        # Save image
+        img_template.save(path.join(PATH_OUTPUT_FOLDER, "image" + str(local_cnt) + ".jpg"))
+
+        local_cnt = local_cnt + 1
+
+if __name__ == "__main__":
+    read_overlay_images()
+    generate_images()
